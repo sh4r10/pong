339b26058427d30ad1e02b79b89f196255d28671
Shariq Shahbaz <iamsh4r10@gmail.com>
2025-11-28 00:21:37 +0100

    improve collision mechanics

--- src/main.c
+++ src/main.c
@@ -1,29 +1,40 @@
 #include "raylib.h"
 
-bool is_ball_colliding(Vector2 box_pos, Vector2 box_dimensions, Vector2 ball_pos, int ball_radius){
-	bool is_within_x_left = ball_pos.x + ball_radius > box_pos.x;
-	bool is_within_x_right = ball_pos.x - ball_radius < box_pos.x + box_dimensions.x;
-	bool is_within_y_left = ball_pos.y + ball_radius > box_pos.y;
-	bool is_within_y_right = ball_pos.y - ball_radius < box_pos.y + box_dimensions.y;
+// configs
+const int WINDOW_WIDTH = 1280;
+const int WINDOW_HEIGHT = 800;
+
+const int BORDER_THICKNESS = 10;
+const int DASH_SIZE = 10;
+const int DASH_SPACE = 10;
+
+const int PADDLE_OFFSET = 20;
+const int PADDLE_WIDTH = 20;
+const int PADDLE_HEIGHT = 120;
+const int PLAYER_MOVEMENT_INTERVAL = 15;
+const int AI_MOVEMENT_INTERVAL = 5;
+
+const int BALL_RADIUS = 10;
+const int BALL_SPEED = 15;
+const int MAX_BALL_SLOPE = 15;
+
+bool is_ball_colliding_with_box(Vector2 box_pos, Vector2 box_dimensions, Vector2 ball_pos){
+	bool is_within_x_left = ball_pos.x + BALL_RADIUS > box_pos.x;
+	bool is_within_x_right = ball_pos.x - BALL_RADIUS < box_pos.x + box_dimensions.x;
+	bool is_within_y_left = ball_pos.y + BALL_RADIUS > box_pos.y;
+	bool is_within_y_right = ball_pos.y - BALL_RADIUS < box_pos.y + box_dimensions.y;
 	return is_within_x_left && is_within_x_right && is_within_y_left && is_within_y_right;
 }
 
+float get_collision_angle(Vector2 paddle_pos, Vector2 ball_pos){
+	float py = paddle_pos.y + (PADDLE_HEIGHT/2.0);
+	float diff_from_paddle_center = py - ball_pos.y;
+	float percentage_diff = (diff_from_paddle_center/PADDLE_HEIGHT);
+	float variation = GetRandomValue(0, 50)/1000.0;
+	return (percentage_diff+variation) * -MAX_BALL_SLOPE;
+}
+
 int main () {
-	// configs
-	const int WINDOW_WIDTH = 1280;
-	const int WINDOW_HEIGHT = 800;
-
-	const int BORDER_THICKNESS = 10;
-	const int DASH_SIZE = 10;
-	const int DASH_SPACE = 10;
-
-	const int PADDLE_OFFSET = 20;
-	const int PADDLE_WIDTH = 20;
-	const int PADDLE_HEIGHT = 100;
-	const int PADDLE_MOVEMENT_INTERVAL = 20;
-
-	const int BALL_RADIUS = 10;
-	const int BALL_SPEED = 5;
 
 	// use vsync of high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
@@ -48,8 +59,8 @@ int main () {
 	};
 
 	Vector2 ball_slope = {
-		GetRandomValue(-2, 2),
-		GetRandomValue(-2, 2)
+		GetRandomValue(0, 1) ? BALL_SPEED : -BALL_SPEED,
+		GetRandomValue(-1*MAX_BALL_SLOPE, MAX_BALL_SLOPE)
 	};
 	
 	// game loop, run until close or escape
@@ -57,66 +68,69 @@ int main () {
 	{
 		if(IsKeyDown(KEY_J)){
 			// check if would hit border
-			if(player_paddle.y + PADDLE_MOVEMENT_INTERVAL <= WINDOW_HEIGHT - BORDER_THICKNESS - PADDLE_HEIGHT){
-				player_paddle.y += PADDLE_MOVEMENT_INTERVAL;
+			if(player_paddle.y < WINDOW_HEIGHT - BORDER_THICKNESS - PADDLE_HEIGHT){
+				bool hitting_edge = player_paddle.y + PLAYER_MOVEMENT_INTERVAL > WINDOW_HEIGHT - BORDER_THICKNESS - PADDLE_HEIGHT;
+				player_paddle.y = hitting_edge ? WINDOW_HEIGHT - BORDER_THICKNESS - PADDLE_HEIGHT : player_paddle.y + PLAYER_MOVEMENT_INTERVAL;
 			}
 		}
 		if(IsKeyDown(KEY_K)){
 			// check if would hit border
-			if(player_paddle.y - PADDLE_MOVEMENT_INTERVAL >= BORDER_THICKNESS){
-				player_paddle.y -= PADDLE_MOVEMENT_INTERVAL;
+			if(player_paddle.y > BORDER_THICKNESS){
+				bool hitting_edge = player_paddle.y - PLAYER_MOVEMENT_INTERVAL < BORDER_THICKNESS;
+				player_paddle.y = hitting_edge ? BORDER_THICKNESS : player_paddle.y - PLAYER_MOVEMENT_INTERVAL;
 			}
 		}
-		
-		ball_pos.x += ball_slope.x * BALL_SPEED;
-		ball_pos.y += ball_slope.y * BALL_SPEED;
+
+		// move ai paddle
+		if(ball_pos.y > ai_paddle.y + PADDLE_HEIGHT){
+			bool hitting_edge = ai_paddle.y + AI_MOVEMENT_INTERVAL > WINDOW_HEIGHT - BORDER_THICKNESS - PADDLE_HEIGHT;
+			ai_paddle.y = hitting_edge ? WINDOW_HEIGHT - BORDER_THICKNESS - PADDLE_HEIGHT : ai_paddle.y + AI_MOVEMENT_INTERVAL;
+		} else if (ball_pos.y < ai_paddle.y) {
+			bool hitting_edge = ai_paddle.y - AI_MOVEMENT_INTERVAL < BORDER_THICKNESS;
+			ai_paddle.y = hitting_edge ? BORDER_THICKNESS : ai_paddle.y - AI_MOVEMENT_INTERVAL;
+		}
+
+
+		ball_pos.x += ball_slope.x;
+		ball_pos.y += ball_slope.y;
 
 		// check border collisions
-		bool is_colliding_top_border = is_ball_colliding(
+		bool is_colliding_top_border = is_ball_colliding_with_box(
 			(Vector2){0.0,0.0}, 
 			(Vector2){WINDOW_WIDTH, BORDER_THICKNESS}, 
-			ball_pos, 
-			BALL_RADIUS
+			ball_pos
 		);
 
-		bool is_colliding_bottom_border = is_ball_colliding(
+		bool is_colliding_bottom_border = is_ball_colliding_with_box(
 			(Vector2) {0.0, (float)WINDOW_HEIGHT-BORDER_THICKNESS},
 			(Vector2){WINDOW_WIDTH, BORDER_THICKNESS}, 
-			ball_pos,
-			BALL_RADIUS
+			ball_pos
 		);
 
-		bool is_colliding_player_paddle = is_ball_colliding(
+		bool is_colliding_player_paddle = is_ball_colliding_with_box(
 			player_paddle,
-			(Vector2) {PADDLE_WIDTH, PADDLE_HEIGHT},
-			ball_pos,
-			BALL_RADIUS
+			(Vector2) {2, PADDLE_HEIGHT},
+			ball_pos
 		); 
 
-		bool is_colliding_ai_paddle = is_ball_colliding(
+		bool is_colliding_ai_paddle = is_ball_colliding_with_box(
 			ai_paddle,
-			(Vector2) {PADDLE_WIDTH, PADDLE_HEIGHT},
-			ball_pos,
-			BALL_RADIUS
+			(Vector2) {2, PADDLE_HEIGHT},
+			ball_pos
 		); 
 		
 		if(is_colliding_top_border || is_colliding_bottom_border){
 			ball_slope.y *= -1;
 		}
 
-		if(is_colliding_player_paddle || is_colliding_ai_paddle){
+		if(is_colliding_player_paddle){
+			ball_slope.y = get_collision_angle(player_paddle, ball_pos);
 			ball_slope.x *= -1;
 		}
 
-		// move ai paddle
-		if(ball_pos.y > ai_paddle.y + PADDLE_HEIGHT){
-			if(ai_paddle.y + PADDLE_MOVEMENT_INTERVAL < WINDOW_HEIGHT-BORDER_THICKNESS){
-				ai_paddle.y += PADDLE_MOVEMENT_INTERVAL;
-			}
-		} else if (ball_pos.y < ai_paddle.y) {
-			if(ai_paddle.y - PADDLE_MOVEMENT_INTERVAL > BORDER_THICKNESS){
-				ai_paddle.y -= PADDLE_MOVEMENT_INTERVAL;
-			}
+		if(is_colliding_ai_paddle){
+			ball_slope.y = get_collision_angle(ai_paddle, ball_pos);
+			ball_slope.x *= -1;
 		}
 
 		// frame starts
