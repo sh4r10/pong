c26738831846c121173ece755da2dc9c42abd9e8
Shariq Shahbaz <iamsh4r10@gmail.com>
2025-11-27 23:05:50 +0100

    add ball collision and movement logic

--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
 #include "raylib.h"
 
+bool is_ball_colliding(Vector2 box_pos, Vector2 box_dimensions, Vector2 ball_pos, int ball_radius){
+	bool is_within_x_left = ball_pos.x + ball_radius > box_pos.x;
+	bool is_within_x_right = ball_pos.x - ball_radius < box_pos.x + box_dimensions.x;
+	bool is_within_y_left = ball_pos.y + ball_radius > box_pos.y;
+	bool is_within_y_right = ball_pos.y - ball_radius < box_pos.y + box_dimensions.y;
+	return is_within_x_left && is_within_x_right && is_within_y_left && is_within_y_right;
+}
+
 int main () {
 	// configs
 	const int WINDOW_WIDTH = 1280;
@@ -13,6 +21,9 @@ int main () {
 	const int PADDLE_WIDTH = 20;
 	const int PADDLE_HEIGHT = 100;
 	const int PADDLE_MOVEMENT_INTERVAL = 20;
+
+	const int BALL_RADIUS = 10;
+	const int BALL_SPEED = 5;
 
 	// use vsync of high DPI displays
 	SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
@@ -30,6 +41,16 @@ int main () {
 		(float)WINDOW_WIDTH - PADDLE_WIDTH - PADDLE_OFFSET,
 		(WINDOW_HEIGHT / 2.0) - (PADDLE_HEIGHT / 2.0)
 	};
+
+	Vector2 ball_pos = {
+		WINDOW_WIDTH/2.0,
+		WINDOW_HEIGHT/2.0
+	};
+
+	Vector2 ball_slope = {
+		GetRandomValue(-2, 2),
+		GetRandomValue(-2, 2)
+	};
 	
 	// game loop, run until close or escape
 	while (!WindowShouldClose())
@@ -46,6 +67,58 @@ int main () {
 				player_paddle.y -= PADDLE_MOVEMENT_INTERVAL;
 			}
 		}
+		
+		ball_pos.x += ball_slope.x * BALL_SPEED;
+		ball_pos.y += ball_slope.y * BALL_SPEED;
+
+		// check border collisions
+		bool is_colliding_top_border = is_ball_colliding(
+			(Vector2){0.0,0.0}, 
+			(Vector2){WINDOW_WIDTH, BORDER_THICKNESS}, 
+			ball_pos, 
+			BALL_RADIUS
+		);
+
+		bool is_colliding_bottom_border = is_ball_colliding(
+			(Vector2) {0.0, (float)WINDOW_HEIGHT-BORDER_THICKNESS},
+			(Vector2){WINDOW_WIDTH, BORDER_THICKNESS}, 
+			ball_pos,
+			BALL_RADIUS
+		);
+
+		bool is_colliding_player_paddle = is_ball_colliding(
+			player_paddle,
+			(Vector2) {PADDLE_WIDTH, PADDLE_HEIGHT},
+			ball_pos,
+			BALL_RADIUS
+		); 
+
+		bool is_colliding_ai_paddle = is_ball_colliding(
+			ai_paddle,
+			(Vector2) {PADDLE_WIDTH, PADDLE_HEIGHT},
+			ball_pos,
+			BALL_RADIUS
+		); 
+		
+		if(is_colliding_top_border || is_colliding_bottom_border){
+			ball_slope.y *= -1;
+		}
+
+		if(is_colliding_player_paddle || is_colliding_ai_paddle){
+			ball_slope.x *= -1;
+		}
+
+		// move ai paddle
+		if(ball_pos.y > ai_paddle.y + PADDLE_HEIGHT){
+			if(ai_paddle.y + PADDLE_MOVEMENT_INTERVAL < WINDOW_HEIGHT-BORDER_THICKNESS){
+				ai_paddle.y += PADDLE_MOVEMENT_INTERVAL;
+			}
+		} else if (ball_pos.y < ai_paddle.y) {
+			if(ai_paddle.y - PADDLE_MOVEMENT_INTERVAL > BORDER_THICKNESS){
+				ai_paddle.y -= PADDLE_MOVEMENT_INTERVAL;
+			}
+		}
+
 		// frame starts
 		BeginDrawing();
 		ClearBackground(BLACK);
@@ -58,6 +131,9 @@ int main () {
 		// draw player paddles
 		DrawRectangle(ai_paddle.x, ai_paddle.y, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE);
 		DrawRectangle(player_paddle.x, player_paddle.y, PADDLE_WIDTH, PADDLE_HEIGHT, WHITE);
+
+		// draw ball
+		DrawCircle(ball_pos.x, ball_pos.y, (float)BALL_RADIUS, WHITE);
 
 		// frame ends
 		EndDrawing();
